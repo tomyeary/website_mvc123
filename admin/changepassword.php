e3ea8ed1d4decc690e15d82afbb3ae31793659d5
jack <lyanhquan20@gmail.com>
2020-07-17 08:23:35 +0700

    s

--- admin/changepassword.php
+++ admin/changepassword.php
@@ -1,5 +1,32 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php 
+    $filepath = realpath(dirname(__FILE__));
+    include_once ($filepath.'/../lib/session.php');
+    include_once ($filepath.'/../lib/database.php');
+    include_once ($filepath.'/../helpers/format.php');
+ ?> 
+<?php
+    if (isset($_POST['submit'])){
+        $matkhaucu=$_POST['password'];
+        $matkhaumoi=$_POST['newpassword'];
+        $sql_doimatkhau = "SELECT * FROM tbl_admin WHERE adminPass = '$matkhaucu' LIMIT 1";
+        $result = $this->db->select($sql_doimatkhau);
+        $get_rows=mysqli_num_rows($sql_doimatkhau);
+        if ($get_rows==0){
+                
+                        $alert = "Sai tài khoản hoặc mật khẩu xin hãy điển lại!";
+                        return $alert;
+            }else{
+                $sql_capnhap="UPDATE tbl_admin set adminPass='$matkhaumoi'";
+                 $result = $this->db->update($sql_capnhap);
+                        $alert = "Bạn đã thay đổi mật khẩu!";
+                        return $alert;
+                }
+        
+        }
+?>
+
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Change Password</h2>
@@ -11,7 +38,7 @@
                         <label>Old Password</label>
                     </td>
                     <td>
-                        <input type="password" placeholder="Enter Old Password..."  name="title" class="medium" />
+                        <input type="password" placeholder="Enter Old Password..."  name="password" class="medium" />
                     </td>
                 </tr>
 				 <tr>
@@ -19,7 +46,7 @@
                         <label>New Password</label>
                     </td>
                     <td>
-                        <input type="password" placeholder="Enter New Password..." name="slogan" class="medium" />
+                        <input type="password" placeholder="Enter New Password..." name="newpassword" class="medium" />
                     </td>
                 </tr>
 				 
--- classes/adminlogin.php
+++ classes/adminlogin.php
@@ -1,6 +1,6 @@
 <?php 
 	$filepath = realpath(dirname(__FILE__));
-	include ($filepath.'/../lib/session.php');
+	include_once	 ($filepath.'/../lib/session.php');
 	Session::checkLogin();
 	include_once ($filepath.'/../lib/database.php');
 	include_once ($filepath.'/../helpers/format.php');
@@ -18,6 +18,36 @@
 			$this->db = new Database();
 			$this->fm = new Format();
 		}
+
+		/*public function admin_changepassword($matkhaucu,$matkhaumoi)
+		{
+			$matkhaucu = $this ->fm ->validation($matkhaucu); 	
+			$matkhaumoi = $this ->fm ->validation($matkhaumoi); 
+			$matkhaucu = mysqli_real_escape_string($this->db->link, $matkhaucu);
+			$matkhaumoi = mysqli_real_escape_string($this->db->link, $matkhaumoi);
+			$query = "SELECT * FROM tbl_admin WHERE adminPass = '$matkhaucu' LIMIT 1";
+			$result = $this->db->select($query);
+			$get_rows = mysqli_num_rows($query);
+			if ($get_rows == 0) 
+			{
+
+				echo '<script>
+						  alert("Sai tài khoản hoặc mật khẩu xin hãy điển lại!")
+						</script>';
+			}else
+			{
+				$capnhap = "UPDATE tbl_admin set adminPass = '$matkhaumoi'";
+				$result = $this->db->select($capnhap);
+				echo '<script>
+						  alert("Bạn đã thay đổi mật khẩu!");
+						</script>';
+			}
+		}
+			*/
+				
+				
+			
+
 
 		public function login_admin($adminUser,$adminPass)
 		{
